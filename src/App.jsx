05de7e8e4bde54f67e48bfc9d01c7a38b819bd58
maks_bezr <maks.bezrodnyi@gmail.com>
2021-09-09 02:28:14 +0300

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
 import GameCanvas from './Containers/GameCanvas/GameCanvas';
-import logo from './images/memory.svg';
+import Header from './Components/Header/Header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <div className="game-logo">
-          <img src={logo} alt="logo" />
-        </div>
-        <h2 className="name">Memory Game</h2>
-      </header>
+      <Header />
       <GameCanvas />
     </div>
   );
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import logo from '../../images/memory.svg';
+import './header.css'
+
+const Header = () => {
+ return (
+  <header className="App-header">
+   <div className="game-logo">
+    <img src={logo} alt="logo" />
+   </div>
+   <h2 className="name">Memory Game</h2>
+  </header>
+ )
+}
+
+export default Header;
