634d46838b40a88b4d1c1dabba4037f893d69c05
maks_bezr <maks.bezrodnyi@gmail.com>
2021-09-10 02:14:02 +0300

    fixed position

--- src/configs.js
+++ src/configs.js
@@ -1,5 +1,5 @@
 const configs = {
-  gameDimension: 3,
+  gameDimension: 2,
   timer: 20,
   analyticsInfo:
   {
@@ -12,24 +12,52 @@ const configs = {
 
 const heroes = [
   {
+    name: 'Star Lord',
+    position: '-28px 23px',
+  },
+  {
+    name: 'Black Widow',
+    position: '-9px -2px',
+  },
+  {
+    name: 'Nebula',
+    position: '8px -2px',
+  },
+  {
+    name: 'War Machine',
+    position: '-26px -2px',
+  },
+  {
+    name: 'Ant-Man',
+    position: '25.5px -2px',
+  },
+  {
+    name: 'Ms. Marvel',
+    position: '8px 23px',
+  },
+  {
+    name: 'Hawkeye',
+    position: '27px 48px',
+  },
+  {
     name: 'Hulk',
-    position: '-20px 22px',
+    position: '-10px 23px',
   },
   {
     name: 'Thanos',
-    position: '18px 22px',
+    position: '27px 23px',
   },
   {
     name: 'Thor',
-    position: '-28px 50px',
+    position: '-25px 48px',
   },
   {
     name: 'Iron Man',
-    position: '11px 50px',
+    position: '9.5px 48px',
   },
   {
     name: 'Captain America',
-    position: '-8px 50px',
+    position: '-7px 48px',
   }]
 
 export { configs, heroes };
